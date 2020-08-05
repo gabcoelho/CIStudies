26d149cfca8086b6e84e9af70ef7c4d83bfbff94
Gabriela Coelho <gabsaac@gmail.com>
2020-08-05 11:13:38 -0300

    Adding test project

---
+++ CIStudies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIStudies
+//
+//  Created by Gabriela Coelho on 05/08/20.
+//  Copyright © 2020 gac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CIStudies' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Beagle', '1.0.0-RC1'
+
+
+  target 'CIStudiesTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
